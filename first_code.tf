218d705fc50f1607c73bb2531a0b8e39fb635a04
MichalBeczek1985 <31369080+MichalBeczek1985@users.noreply.github.com>
2022-02-14 14:09:42 +0100

    dynamo ttl add

--- first_code.tf
+++ first_code.tf
@@ -1,9 +1,28 @@
-proviader "aws"{
+provider "aws"{
     profile = "default"
     region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "tf_course" {
-    bucket  = "tf-michal-20220214"
-    acl     = "private"
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "GameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
 }
